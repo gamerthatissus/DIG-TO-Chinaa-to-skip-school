3992834fa47be32d3ff1a108c6b31b114abb90c1
gamerthatissus <123601491+gamerthatissus@users.noreply.github.com>
2025-02-11 12:54:17 -0800

    hi
    hi

--- CHINA DIG/Assets/move.cs
+++ CHINA DIG/Assets/move.cs
@@ -5,19 +5,75 @@ using UnityEngine;
 public class move : MonoBehaviour
 {
 
-    public Rigidbody MyRigidbody2d;
-        
-    #endregion
+    public Rigidbody myRigidbody;
+     public CapsuleCollider myCapsuleCollider;
+
+     private bool isgrounded;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       isgrounded=IsGrounded();
+       
+        if (Input.GetKeyDown(KeyCode.Space) == true && isgrounded==true) 
+        {
+myRigidbody.velocity=Vector3.up*10;
+        }
+       
+       
+       
+       
+        if (Input.GetKeyDown(KeyCode.W) == true) 
+        {
+
+        }
+
+
+ if (Input.GetKeyDown(KeyCode.A) == true) 
+ {
+
+ }
+ 
+ if (Input.GetKeyDown(KeyCode.S) == true) 
+ {
+
+
+ }
+
+ if (Input.GetKeyDown(KeyCode.D) == true)
+ {
+
+
+ }
+ 
+
     }
+
+    private bool IsGrounded()
+{
+
+RaycastHit hit;
+float dis=0.1f; 
+
+
+if (Physics.Raycast(myCapsuleCollider.bounds.center, Vector3.down, out hit, myCapsuleCollider.bounds.extents.y + dis))
+{
+
+return true;
+
+}else{
+
+return false;
+    
 }
+
+}
+
+}
+
