46b35f79cb755fe0245d094b9362c199c421f933
gamerthatissus <123601491+gamerthatissus@users.noreply.github.com>
2025-02-12 12:31:17 -0800

    fd
    g

--- CHINA DIG/Assets/move.cs
+++ CHINA DIG/Assets/move.cs
@@ -7,18 +7,22 @@ public class move : MonoBehaviour
 
     public Rigidbody myRigidbody;
      public CapsuleCollider myCapsuleCollider;
+public Transform myTransform;
 
+public float MyRotation;
      private bool isgrounded;
 
     // Start is called before the first frame update
     void Start()
     {
+                MyRotation=myTransform.rotation.x;
 
     }
 
     // Update is called once per frame
     void Update()
     {
+myTransform.rotation.x=MyRotation;
        isgrounded=IsGrounded();
        
         if (Input.GetKeyDown(KeyCode.Space) == true && isgrounded==true) 
@@ -37,7 +41,7 @@ myRigidbody.velocity=Vector3.up*10;
 
  if (Input.GetKeyDown(KeyCode.A) == true) 
  {
-
+    GameObject.Transform.rotation.x=GameObject.Transform.rotation.x+1;
  }
  
  if (Input.GetKeyDown(KeyCode.S) == true) 
