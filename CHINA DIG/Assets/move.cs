b8dc6bfcb6f07d96560f8fb8ae2ecf163d748eaa
gamerthatissus <123601491+gamerthatissus@users.noreply.github.com>
2025-02-24 12:41:08 -0800

    cc
    cc

--- CHINA DIG/Assets/move.cs
+++ CHINA DIG/Assets/move.cs
@@ -6,9 +6,9 @@ public class move : MonoBehaviour
 {
 
     public Rigidbody myRigidbody;
-     public BoxCollider myCapsuleCollider;
+    public BoxCollider myCapsuleCollider;
 
-     private bool isgrounded;
+    private bool isgrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -19,69 +19,65 @@ public class move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       isgrounded=IsGrounded();
+        isgrounded=IsGrounded();
        
         if (Input.GetKeyDown(KeyCode.Space) == true && isgrounded==true) 
         {
-myRigidbody.velocity=Vector3.up*10;
+            myRigidbody.velocity=Vector3.up*10;
         }
        
        
        
-       
-        if (Input.GetKeyDown(KeyCode.W) == true) 
+        if (Input.GetKey(KeyCode.W) == true) 
         {
- 
+myRigidbody.MovePosition(myRigidbody.position+transform.forward * (10 * Time.deltaTime));
         }
-
-
- if (Input.GetKey(KeyCode.RightArrow))
- {
-    myRigidbody.MoveRotation(myRigidbody.rotation * Quaternion.Euler(0, -35 * Time.deltaTime, 0));
- }     
-
- 
- if (Input.GetKeyDown(KeyCode.D) == true) 
- {
-     
-     
-     if (Input.GetKeyDown(KeyCode.A) == true) 
-     {
-  
-
-
- }
-
- if (Input.GetKey(KeyCode.LeftArrow) == true)
- {
+        
+      
+        if (Input.GetKey(KeyCode.D) == true)
+        {
     
-     myRigidbody.MoveRotation(myRigidbody.rotation * Quaternion.Euler(0, 35 * Time.deltaTime, 0));
 
 
- }
+        }
+        
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+    
+            myRigidbody.MoveRotation(myRigidbody.rotation * Quaternion.Euler(0, -35 * Time.deltaTime, 0));
+
+
+        }
+        
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+    
+            myRigidbody.MoveRotation(myRigidbody.rotation * Quaternion.Euler(0, 35 * Time.deltaTime, 0));
+
+
+        }
  
 
     }
 
     private bool IsGrounded()
-{
+    {
 
-RaycastHit hit;
-float dis=0.1f; 
+        RaycastHit hit;
+        float dis=0.1f; 
 
 
-if (Physics.Raycast(myCapsuleCollider.bounds.center, Vector3.down, out hit, myCapsuleCollider.bounds.extents.y + dis))
-{
+        if (Physics.Raycast(myCapsuleCollider.bounds.center, Vector3.down, out hit, myCapsuleCollider.bounds.extents.y + dis))
+        {
 
-return true;
+            return true;
 
-}else{
+        }else{
 
-return false;
+            return false;
     
-}
+        }
+
+    }
 
 }
-
-}
-
