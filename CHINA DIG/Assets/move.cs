41a58fa65125bc393ea445f8c21136dc9ebae4ee
gamerthatissus <123601491+gamerthatissus@users.noreply.github.com>
2025-02-25 12:30:33 -0800

    fff
    ff

--- CHINA DIG/Assets/move.cs
+++ CHINA DIG/Assets/move.cs
@@ -8,6 +8,7 @@ public class move : MonoBehaviour
     public Rigidbody myRigidbody;
     public BoxCollider myCapsuleCollider;
     public AudioSource mysound;
+    public AudioSource mysound2;
 
     
     private bool isgrounded;
@@ -15,7 +16,7 @@ public class move : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+mysound2.Play();
     }
 
     // Update is called once per frame
@@ -27,18 +28,29 @@ public class move : MonoBehaviour
         {
             AudioSource.PlayClipAtPoint(mysound.clip,transform.position);
             myRigidbody.velocity=Vector3.up*10;
+            
         }
-       
-       
-       
+
+        if (myRigidbody.position.y < -20)
+        {
+            myRigidbody.rotation=Quaternion.Euler(0,0,0);
+
+            myRigidbody.position = new Vector3(0, 20, 0);
+        }
+
         if (Input.GetKey(KeyCode.W) == true) 
         {
-myRigidbody.MovePosition(myRigidbody.position+transform.forward * (10 * Time.deltaTime));
+myRigidbody.MovePosition(myRigidbody.position+transform.forward * (15 * Time.deltaTime));
         }
         
         if (Input.GetKey(KeyCode.S) == true) 
         {
             myRigidbody.MovePosition(myRigidbody.position+transform.forward * (-10 * Time.deltaTime));
+        }
+        
+        if (Input.GetKey(KeyCode.Mouse1) == true && isgrounded==true) 
+        {
+            
         }
         
         if (Input.GetKey(KeyCode.D) == true)
